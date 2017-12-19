a6ebcd4dd26659507cbfc8ab4b66abc492ac80b2
Colton Somes <coltonsomes@gmail.com>
2017-12-18 17:30:45 -0800

    change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -43,7 +43,6 @@ class HomeScreen extends Component {
     let g = 100;
     return (
       <View style={styles.container} >
-<<<<<<< HEAD
         <View style={styles.tabContainer} >
           <TabBar changeTab={(index) => this._changeTab(index)} leftOnPress={() => this._presentFilterModal() } />
         </View>
@@ -52,15 +51,6 @@ class HomeScreen extends Component {
           <FilterModal />
         </Modal>
 
-=======
-      {//  <View style={styles.tabContainer} >
-          // <TabBar changeTab={(index) => this._changeTab(index)}/>
-
-
-        //</View>
-        }
-        <RestaurantScreen/>
->>>>>>> colt-dev
       </View>
     )
   }
